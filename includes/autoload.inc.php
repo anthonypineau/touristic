5954f06222f835396c267c0d14958c11bb70b0aa
Anthony Pineau <78272864+anthonypineau@users.noreply.github.com>
2021-09-07 16:11:12 +0200

    commit

--- includes/autoload.inc.php
+++ includes/autoload.inc.php
@@ -11,7 +11,6 @@ function chargeurBaseEspacesDeNoms($className)
 {
 //    $className = ltrim($className, '\\');
     $fileName  = __DIR__.'/../';
-
     $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $className).'.class.php';
     if (file_exists($fileName)) {
         require_once($fileName);
